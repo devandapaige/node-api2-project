7595dd2673fea22c9dbc4c21e9733a7ec97e402f
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-15 17:02:41 -0500

    had to reinstall dependencies to allow server to start running porperly

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,11 @@
 // implement your posts router here
+const express = require("express");
+const posts = require("./posts-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({ message: "node-api2-project posts-router project api"})
+})
+
+module.exports = router;
